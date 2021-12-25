2e49eb2df458f18429eb604f6d43c7e704677c61
Sabrinapope.md <40004402+Sabrinapope@users.noreply.github.com>
2021-12-25 14:26:31 -0500

    Complete solution

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+Exercise Lab 1:
+  
 names_list = ["Rickey", "Bobby", "Micheal", "Ronnie", "Johnny", "Ralph"]
 names_list.append("Ben")
 names_list.insert(3, 'Jerry')
@@ -12,3 +14,30 @@ del names_list[5]
 names_list = ["Rickey", "Bobby", "Micheal", "Johnny", "Ralph", "Ben", "Jerry"]
 names_list = sorted(names_list)
 print(names_list)
+
+Exercise Lab 2:
+  
+num_list = ['0','1','2','3','4']
+print(num_list)
+
+num_list.append('5')
+print(num_list)
+
+num_list = ['0','1','2','3','4']
+num_list.insert(0,7)
+print(num_list)
+
+num_list = ['0','1','2','3','4']
+del num_list[2]
+print(num_list)
+
+num_list = ['0','1','2','3','4']
+num_list.sort(unsort=True)
+print(num_list)
+
+
+num_list = ['5','6','7', '8','9']
+print(num_list)
+
+num_list = ['0','1','2','3','4','5','6','7','8','9']
+print(num_list)
